7605ea84313bbc7597f8eada7c39b65cc1da3065
Franciscofmv <francisco.moyet@outlook.com>
2025-11-15 21:25:42 -0500

    Challenge 2 Complete: prime finder

--- main.cpp
+++ main.cpp
@@ -32,6 +32,7 @@ Write a function that prints all the prime numbers in an array of integers.
 If a prime is found, add the prime to the vector passed as argument. 
 ************************************************************************************************/
 bool isPrime(const int &p){
+        // function to check if numer is primer.
     if (p < 2)
         return false;
         // Using Wilson theorem (p-1)! congruent with -1 mod p:
@@ -40,6 +41,7 @@ bool isPrime(const int &p){
     size_t i = 1;
     while (i <= (p-1)){
         y *= i;
+        i += 1;
     }
         // Apply Wilson theorem:
     if ((y + 1) % p == 0){
@@ -49,19 +51,37 @@ bool isPrime(const int &p){
         return false;
 }
 void findPrimes(const int arr[], size_t arr_size, std::vector<int> &primes){
+        // function to append the prime numbers to a vector
     for(size_t i = 0; i < arr_size; ++i){
-        if (isPrime(arr[0])){
-            
+        int pos = 0;
+        int p = arr[i];
+        if (isPrime(p)){
+            cout << p << " is prime!" << endl;
+            primes.push_back(p);
+            pos += 1;
         }
     }
 };
 
 int main(){
     // Challenge 1:
-    // int arr[] = {1,2,3,4,5,6,7,8};
-    // int arr_size = 8;
-    // cout << "The sum is " << calculateSum(arr, arr_size) << endl;
-    // return 0;
+    /* 
+    int arr[] = {1,2,3,4,5,6,7,8};
+    int arr_size = 8;
+    cout << "The sum is " << calculateSum(arr, arr_size) << endl;
+    */
     //*********************************************************************
-    // Challenge 2: Wri
+    // Challenge 2:
+    std::vector<int> primes;
+    cout << "Test"<<endl;
+    int test_list[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+
+    findPrimes(test_list, 15, primes);
+    size_t n = primes.size();
+    cout << "There are "<< n << " primes"<< endl;
+    
+    for (size_t k = 0; k < n; ++k){
+        cout << "Primes are: "<< primes[k] << endl;
+    }
+    return 0;
 }
